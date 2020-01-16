a1a95e7f4eafa732d35939f0dc80b2d2d28c3625
sailfish009 <sailfish009@gmail.com>
2020-01-16 18:30:56 +0900

    update

--- custom/impl.py
+++ custom/impl.py
@@ -1,6 +1,7 @@
 import torch
 from .. utils.logger import setup_logger
 from .. utils.collect_env import collect_env_info
+from . ml_stratifiers import MultilabelStratifiedShuffleSplit
 
 def get_cuda_count():
     return torch.cuda.device.count()
@@ -13,3 +14,15 @@ def get_logger(cfg, save_path="", distributed_rank=0):
     logger.info("\n" + collect_env_info())
     return logger
 
+def train_test_split(df, n_splits=4):
+    df_backup = df.copy()
+    X = df['Id'].tolist()
+    y = df['target_vec'].tolist()
+    msss = MultilabelStratifiedShuffleSplit(n_splits=n_splits, test_size=0.1, random_state=42)
+    train_dfs, valid_dfs = [], []
+    for train_index, valid_index in msss.split(X,y):
+        train_df = df_backup.iloc[train_index]
+        train_dfs.append(train_df[['Id', 'Target']])
+        valid_df = df_backup.iloc[valid_index]
+        valid_dfs.append(valid_df[['Id', 'Target']])
+    return train_dfs, valid_dfs
